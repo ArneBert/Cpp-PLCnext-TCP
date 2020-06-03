780aba4efefcb90d7df41925c3a9d53cd5ff3fb2
ArneBert <a.berteloot@hotmail.com>
2020-06-03 14:38:15 +0200

    Update CppRSCServiceProgram.cpp

--- CppRSCService/src/CppRSCServiceProgram.cpp
+++ CppRSCService/src/CppRSCServiceProgram.cpp
@@ -9,12 +9,6 @@ void CppRSCServiceProgram::Execute()
 {
     //implement program 
  // Something has to follow here, but I'm not quiet sure what.
- 	 uint32 dword;
-	 RscString<512> dwordstr;
-	 dwordstr = "Arp.Plc.Eclr/main1.DW_IN1";
-
-	 ReadSingle(dwordstr);
-
 }
 
 } // end of namespace CppRSCService
