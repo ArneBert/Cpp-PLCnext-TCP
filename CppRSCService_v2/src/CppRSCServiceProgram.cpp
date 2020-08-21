259055f8200aa002ce0af20b21283436c1f957da
ArneBert <a.berteloot@hotmail.com>
2020-08-21 14:48:46 +0200

    Update CppRSCServiceProgram.cpp

--- CppRSCService_v2/src/CppRSCServiceProgram.cpp
+++ CppRSCService_v2/src/CppRSCServiceProgram.cpp
@@ -1,4 +1,4 @@
-﻿#include "CppRSCServiceProgram.hpp"
+#include "CppRSCServiceProgram.hpp"
 #include "Arp/System/Commons/Logging.h"
 #include "Arp/System/Core/ByteConverter.hpp"
 
@@ -9,6 +9,13 @@ namespace CppRSCService
 void CppRSCServiceProgram::Execute()
 {
     //implement program 
+	memcpy(cppRSCServiceComponent.ports.InField, InBuffer, sizeof(InBuffer));
+	memcpy(OutBuffer,cppRSCServiceComponent.ports.OutField, sizeof(OutBuffer));
+	// = cppRSCServiceComponent.startTime
+	OUTstart = cppRSCServiceComponent.ports.int64startTime2;
+	OUTactive = cppRSCServiceComponent.ports.int64activeTime2;
+	OUTend 		= cppRSCServiceComponent.ports.int64endTime2;
+	//OutBuffer = cppRSCServiceComponent.arrayReader;
 
 }
 
